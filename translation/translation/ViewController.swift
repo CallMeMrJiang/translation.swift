b17c6d19c4c77bfed46a834b48d618664e0eb03e
Mr.Jiang <jasonryan@MrJiangdeMacBook-Pro.local>
2015-12-21 09:30:51 +0800

    first commit

---
+++ translation/translation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  translation
+//
+//  Created by Mr.Jiang on 15/12/21.
+//  Copyright © 2015年 JiangBingbing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
